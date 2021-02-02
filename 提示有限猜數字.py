9fed8a70fd2807f12279418e459f1e7fdfcc6ee7
yuyun0721 <78334490+yuyun0721@users.noreply.github.com>
2021-02-02 17:06:30 +0800

    Add files via upload

--- 提示有限猜數字.py
+++ 提示有限猜數字.py
@@ -1,7 +1,7 @@
 y=int(input('請輸入最大上限'))
 x=int(input('請輸入最小下限'))
 import random
-num=random.randint(y,x)
+num=random.randint(x,y)
 a=int(input('請問你想要有幾次機會?'))
 c=int(input('請輸入數字'))
 for c in range(a):
@@ -10,8 +10,10 @@ for c in range(a):
     elif (c>num):
         a=a-1
         print('太大了，小一點，你還有',a,'次機會')
-    else :
+        c=int(input('請再輸入數字'))
+    elif (c<num):
        a=a-1
        print('太小了，大一點，你還有次，',a,'機會')
-else:
-    print('輸入錯誤，請再試一次')
+       c=int(input('請再輸入數字'))
+    else:
+       print('輸入錯誤，請再試一次')
