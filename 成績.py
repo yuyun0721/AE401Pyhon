05c369a815c788826d19d7595e64c0ef2a0a11c2
yuyun0721 <78334490+yuyun0721@users.noreply.github.com>
2021-02-02 08:51:26 +0800

    Add files via upload

---
+++ 成績.py
@@ -0,0 +1,12 @@
+score=input('請輸入成績')
+score=int(score)
+if score>89:
+    print('A')
+elif score>79:
+    print('B')
+elif score>69:
+    print('C')
+elif score>59:
+    print('D')
+else:
+    print('E')
